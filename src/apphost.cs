810eff731421ef7453a1bf58372c6e5acf2c9e08
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-12-29 14:46:16 -0500

    Started layout

---
+++ src/apphost.cs
@@ -0,0 +1,10 @@
+﻿#:package CommunityToolkit.Aspire.Hosting.Ollama@13.0.1-beta.468
+#:sdk Aspire.AppHost.Sdk@13.1.0
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var ollama = builder.AddOllama("ollama");
+
+var gemma = ollama.AddModel("gemma", "gemma3:1b");
+
+builder.Build().Run();
