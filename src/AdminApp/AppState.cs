76c40e93780cbd1eb536608b088dae6794efdc3a
Carl Franklin <carl@franklins.net>
2026-01-11 14:49:41 -0500

    Cleaned up home page. Automatically saving the schema. Added load and delete buttons and a configure button which navigateis to a new config page

---
+++ src/AdminApp/AppState.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminApp;
+
+public class AppState
+{
+	public string SaveFilePath { get; set; } = string.Empty;
+	public string SchemaJson { get; set; } = string.Empty;
+
+}
--- src/AdminApp/Program.cs
+++ src/AdminApp/Program.cs
@@ -1,3 +1,4 @@
+using AdminApp;
 using AdminApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
+
+builder.Services.AddScoped<AppState>();
 
 builder.Services.AddSingleton<QueryGenerator.Generator>();
 
