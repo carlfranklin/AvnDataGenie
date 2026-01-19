21fdf9c585859456848ebebce89f2aabddb627db
Carl Franklin <carl@franklins.net>
2026-01-19 12:58:38 -0500

    Added Connection String to AppState

--- src/AdminApp/AppState.cs
+++ src/AdminApp/AppState.cs
@@ -79,6 +79,7 @@ public class AppState
 
 	public string SaveFilePath { get; set; } = string.Empty;
 	public string SchemaJson { get; set; } = string.Empty;
+	public string ConnectionString { get; set; } = string.Empty;
 	public DatabaseSchema? DatabaseSchema { get; set; }
 	public LlmConfiguration LlmConfiguration { get; set; } = new();
 	public string ConfigSaveFilePath { get; set; } = string.Empty;
